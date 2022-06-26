dc3c1d4e245d526979633cfeea32dcabcd029d17
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-06-26 13:32:52 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
+let lastBmi=0;
+let lastWeight=0;
+
+/* let bmiWertCount = 1
+
+const addBmiWert = (number, date = new Date())=>{
+    const datumBmiWert.createElement("p")
+    datumBmiWert = date
+    const saveBmiLast.createElement("p")
+    const lastBmi = document.querySelector("#BMIwert").value;
+    const newBmiWert${number}
+    console.log
+    bmiWertCount++
+}
+
+addBmiWert(bmiWertCount) */
+
 
 
 function berechnung () {
@@ -9,19 +26,16 @@ function berechnung () {
     const prev = document.querySelector("#show")
     if (alter == 0 || gewicht == 0 || größe == 0)
     {alert("Ihre Eingabe ist nicht korrekt"); return}
-    //const alterValue = alter.value
-    //const gewichtValue = gewicht.value
-    //const größeValue = größe.value
-    //console.log(sex, alter, gewicht)
-    //const sexValue = sex.value
     let result = gewicht/((größe/100)**2)
     BMIwert.innerHTML=result.toFixed(2);
     let resumee = document.createElement("p");
     resumee =`Sie sind ${alter} Jahre alt,\n 
     Ihre Größe ist ${größe/100} m\n
     und sie wiegen derzeit ${gewicht} Kilogramm.`;
-    prev.before(resumee);
+    let zusammenfassungEingabe=document.querySelector("#zusammenfassungEingabe")
+    zusammenfassungEingabe.textContent=resumee;
     //text.classList.add("add");
+    compareBMI(result.toFixed(2),gewicht)
     const auswertung=document.querySelector('#auswertung')
     auswertung.style.display=("inline-block")
 }
@@ -29,16 +43,49 @@ function berechnung () {
 const button = document.querySelector("button")
 button.addEventListener('click', berechnung)
 
-async function evaluation() {
+/* async function evaluation() {
     
-}
+} */
 
 /* function toggleSex() {
     {
         on: 'männlich',
         off: 'weiblich'
-      });
+    });
 }
 const sexToggle = document.querySelector('#toggle_sex')
 sexToggle.classList.add('.bootstrapToggle')
 sexToggle.addEventListener('click', toggleSex()) */
+
+const saveBmiLast = (latest,gewicht) => {
+    const lastBmiWert = latest;
+    const lastWeightWert = gewicht
+    localStorage.setItem("last_BMI", latest);
+    localStorage.setItem("Datum", new Date());
+    localStorage.setItem("last_Weight", gewicht)
+    console.log(lastBmiWert, lastWeightWert)
+};
+    
+const compareBMI = (newBmiValue) => {
+    const lastBmi = localStorage.getItem("last_BMI");
+    const lastDate = localStorage.getItem("Datum");
+    const lastWeight = localStorage.getItem("last_Weight");
+    const newBmi = newBmiValue
+    const newWeight = gewicht
+
+    console.log(lastBmi, newBmi)
+    let text = ""
+    const differenzKg = newWeight - lastWeight
+    const differenzBmi = newBmi - lastBmi
+        if (newBmi > lastBmi) { 
+            text= `Sie haben Ihr Gewicht um ${differenzKg.toFixed(1)} kg aufgebaut. Ihr BMI-Wert ist damit um ${differenzBmi.toFixed(1)} Einheiten gestiegen`
+        } else if (newBmi === lastBmi) {
+            text= "Ihr BMI hat sich nicht verändert, weil sie Ihr Gewicht gehalten haben"
+        } else {
+            text= `Sie haben Ihr Gewicht um ${differenzKg.toFixed(1)*-1} kg abgebaut. Ihr BMI-Wert ist damit um ${differenzBmi.toFixed(1)*-1} Einheiten gefallen`
+        }
+        document.querySelector("#vergleich").innerHTML = text;
+        saveBmiLast(newBmi)
+  };
+
+    
